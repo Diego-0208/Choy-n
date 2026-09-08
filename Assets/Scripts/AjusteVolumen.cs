6387b2c9abb79f041dbc6426c2315f0edcb2b56b
Diego-0208 <alveardiego16@gmail.com>
2026-09-08 14:58:43 -0300

    creación del sctip del volumen

--- Assets/Scripts/AjusteVolumen.cs
+++ Assets/Scripts/AjusteVolumen.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class AjusteVolumen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [header("Referencia del UI volumen") ]
+    [serializeField] private Slider sliderMusica;  
+    [serializeField] private Slider sliderFX;
+    [serializeField] private Toggle togleMute; 
+
+    private const string  MUSIC_KEY  = "VolumenMusica"; 
+    private const string FX_KEY  = "VolumenFX"; 
+    private const string MUTE_KEY = "Muteado"; 
+    
+    private void Start()
     {
-        
+        CargarAjustes();  
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CargarAjustes() 
     {
-        
+        float musicaVal = PlayerPrefs.GetFloat(MUSIC_KEY,1f); 
+        float fxVAL = PlayerPrefs.GetFloat(FX_KEY,1f); 
+
+        bool isMuted = PlayerPrefs.GetIn(MUTE_KEY,0) ==1; 
+
+        sliderMusica.value = musicaVal; 
+        sliderFX.value = fxVAL;
+        toggleMute.isON = isMuted;
+
+        AplicarCambiosAudio();  
     }
+
+    public void SetVolumenMusica(float valor)
+    {
+        PlayerPrefs.SetFloat(MUSIC_KEY,valor); 
+        PlayerPrefs.Save(); 
+        AplicarCambiosAudio();
+    }
+
+    Public void SetVolumenFX(float valor)
+    {
+        PlayerPrefs.SetFloat(FX_KEY,valor)
+        PlayerPrefs.Save(); 
+        AplicarCambiosAudio; 
+    }
+
+    public void 
 }
